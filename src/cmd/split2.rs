f149ef8498a159f23acc8efa83772e8fddd33d8a
sharkLoc <mmtinfo@163.com>
2025-07-13 16:43:37 +0800

    use paraseq in cmd split2

--- src/cmd/split2.rs
+++ src/cmd/split2.rs
@@ -1,8 +1,10 @@
-use crate::cmd::wrap::*;
-use crate::errors::FakitError;
-use crate::utils::*;
-use bio::io::fasta;
-use log::*;
+use crate::{
+    cmd::wrap::write_record,
+    errors::FakitError,
+    utils::{file_reader, file_writer},
+};
+use log::info;
+use paraseq::fasta::{Reader, RecordSet};
 use std::path::Path;
 
 #[allow(clippy::too_many_arguments)]
@@ -16,13 +18,8 @@ pub fn split_chunk<P: AsRef<Path> + Copy>(
     line_width: usize,
     compression_level: u32,
 ) -> Result<(), FakitError> {
-    let fa_reader = fasta::Reader::new(file_reader(file)?);
-
-    if let Some(file) = file {
-        info!("reading from file: {:?}", file.as_ref());
-    } else {
-        info!("reading from stdin");
-    }
+    let mut fa_reader = file_reader(file).map(Reader::new)?;
+    let mut rset = RecordSet::default();
     let mut n = 0;
     if gzip {
         n += 1;
@@ -41,50 +38,41 @@ pub fn split_chunk<P: AsRef<Path> + Copy>(
     let (mut flag, mut index) = (0usize, 0usize);
     let out = if gzip {
         format!("{}{}.fasta.gz", out_pre, index)
-        //PathBuf::from("./").join(format!("{}{}.fasta.gz", out_pre, index))
     } else if bzip2 {
         format!("{}{}.fasta.bz2", out_pre, index)
-        //PathBuf::from("./").join(format!("{}{}.fasta.bz2", out_pre, index))
     } else if xz {
         format!("{}{}.fasta.xz", out_pre, index)
-        //PathBuf::from("./").join(format!("{}{}.fasta.xz", out_pre, index))
     } else {
         format!("{}{}.fasta", out_pre, index)
-        //PathBuf::from("./").join(format!("{}{}.fasta", out_pre, index))
     };
 
-    let mut fh = vec![fasta::Writer::new(file_writer(
-        Some(&out),
-        compression_level,
-    )?)];
+    let mut fh = vec![file_writer(Some(&out), compression_level)?];
 
     info!("start to write file: {}", out);
-    for rec in fa_reader.records().flatten() {
-        let seq_new = wrap_fasta(rec.seq(), line_width)?;
-        if flag < num {
-            let fhthis = fh.get_mut(index).unwrap();
-            fhthis.write(rec.id(), rec.desc(), seq_new.as_slice())?;
-            flag += 1;
-        } else {
-            index += 1;
-            let out = if gzip {
-                format!("{}{}.fasta.gz", out_pre, index)
-            } else if bzip2 {
-                format!("{}{}.fasta.bz2", out_pre, index)
-            } else if xz {
-                format!("{}{}.fasta.xz", out_pre, index)
+    while rset.fill(&mut fa_reader)? {
+        for rec in rset.iter().map_while(Result::ok) {
+            if flag < num {
+                let mut fhthis = fh.get_mut(index).unwrap();
+                write_record(&mut fhthis, rec.id(), &rec.seq(), line_width)?;
+                flag += 1;
             } else {
-                format!("{}{}.fasta", out_pre, index)
-            };
-            fh.push(fasta::Writer::new(file_writer(
-                Some(&out),
-                compression_level,
-            )?));
-            let fhthis = fh.get_mut(index).unwrap();
+                index += 1;
+                let out = if gzip {
+                    format!("{}{}.fasta.gz", out_pre, index)
+                } else if bzip2 {
+                    format!("{}{}.fasta.bz2", out_pre, index)
+                } else if xz {
+                    format!("{}{}.fasta.xz", out_pre, index)
+                } else {
+                    format!("{}{}.fasta", out_pre, index)
+                };
+                fh.push(file_writer(Some(&out), compression_level)?);
+                let mut fhthis = fh.get_mut(index).unwrap();
 
-            info!("start to write file: {}", out);
-            fhthis.write(rec.id(), rec.desc(), seq_new.as_slice())?;
-            flag = 1; // already write one record in this loop, flag add one
+                info!("start to write file: {}", out);
+                write_record(&mut fhthis, rec.id(), &rec.seq(), line_width)?;
+                flag = 1; // already write one record in this loop, flag add one
+            }
         }
     }
 
