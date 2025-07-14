3516625a5fe596d2f9ba55aa5cc12a2c1f927bb7
sharkLoc <mmtinfo@163.com>
2025-07-14 10:49:27 +0800

    update grep cmd

--- src/cmd/grep.rs
+++ src/cmd/grep.rs
@@ -1,8 +1,13 @@
-use crate::cmd::wrap::*;
-use crate::errors::FakitError;
-use crate::utils::*;
-use bio::io::fasta;
-use log::*;
+use crate::{
+    cmd::wrap::write_record,
+    errors::FakitError,
+    utils::{file_reader, file_writer},
+};
+use log::{error, info};
+use paraseq::{
+    fasta::{Reader, RecordSet},
+    fastx::Record,
+};
 use regex::RegexBuilder;
 use std::path::Path;
 
@@ -17,68 +22,54 @@ pub fn grep_fasta<P: AsRef<Path> + Copy>(
     line_width: usize,
     compression_level: u32,
 ) -> Result<(), FakitError> {
-    let mut fo = file_writer(out, compression_level).map(fasta::Writer::new)?;
+    let mut reader = file_reader(file).map(Reader::new)?;
+    let mut rset = RecordSet::default();
+    let mut writer = file_writer(out, compression_level)?;
 
-    if let Some(file) = file {
-        info!("reading from file: {:?}", file.as_ref());
-    } else {
-        info!("reading from stdin");
-    }
-
-    let mut n = 0usize;
+    let mut flag = 0usize;
     if by_id {
-        n += 1;
+        flag += 1;
     }
     if by_seq {
-        n += 1;
+        flag += 1;
     }
-    match n {
-        1 => (),
-        0 => {
-            error!("please specifiy one of the flags: -n or -s");
-            std::process::exit(1);
-        }
-        _ => {
-            error!("only one of the flags -n (--by-name) or -s (--by-seq), is allowed");
-            std::process::exit(1);
-        }
-    }
-    info!("regex pattern is: {}", pat);
-    let mut n = 0usize;
 
+    if flag == 0 {
+        error!("please specify one of the flags: -n (--by-name) or -s (--by-seq)");
+        std::process::exit(1);
+    } else if flag > 1 {
+        error!("only one of the flags -n (--by-name) or -s (--by-seq), is allowed");
+        std::process::exit(1);
+    }
+
+    info!("regex pattern is: {}", pat);
     let re = RegexBuilder::new(pat)
         .case_insensitive(case)
         .unicode(true)
         .build()?;
-
+    let mut counter = 0usize;
     if by_seq {
-        let fp = file_reader(file).map(fasta::Reader::new)?;
-        for rec in fp.records().flatten() {
-            let seq_str = std::str::from_utf8(rec.seq())?;
-            if re.captures(seq_str).is_some() {
-                n += 1;
-                let seq_new = wrap_fasta(rec.seq(), line_width)?;
-                fo.write(rec.id(), rec.desc(), seq_new.as_slice())?;
+        while rset.fill(&mut reader)? {
+            for rec in rset.iter().map_while(Result::ok) {
+                if re.captures(&rec.seq_str()).is_some() {
+                    counter += 1;
+                    write_record(&mut writer, rec.id(), &rec.seq(), line_width)?;
+                }
             }
         }
     }
     if by_id {
-        let fp = file_reader(file).map(fasta::Reader::new)?;
-        for rec in fp.records().flatten() {
-            let name = if let Some(desc) = rec.desc() {
-                format!("{} {}", rec.id(), desc)
-            } else {
-                rec.id().to_owned()
-            };
-            if re.captures(&name).is_some() {
-                n += 1;
-                let seq_new = wrap_fasta(rec.seq(), line_width)?;
-                fo.write(rec.id(), rec.desc(), seq_new.as_slice())?;
+        while rset.fill(&mut reader)? {
+            for rec in rset.iter().map_while(Result::ok) {
+                if re.captures(rec.id_str()).is_some() {
+                    counter += 1;
+                    write_record(&mut writer, rec.id(), &rec.seq(), line_width)?;
+                }
             }
         }
     }
-    fo.flush()?;
+    writer.flush()?;
 
-    info!("total match sequences number: {}", n);
+    info!("total match sequences number: {}", counter);
     Ok(())
 }
--- src/cmd/wrap.rs
+++ src/cmd/wrap.rs
@@ -1,34 +1,6 @@
 use crate::errors::FakitError;
 use std::io::Write;
 
-#[inline]
-pub fn wrap_fasta(seq_slice: &[u8], line_width: usize) -> Result<Vec<u8>, FakitError> {
-    let seq_len = seq_slice.len();
-    let mut seq_new: Vec<&[u8]> = vec![];
-    let mut index = 0usize;
-
-    loop {
-        index += line_width;
-        let start = index - line_width;
-        if index < seq_len {
-            let window = &seq_slice[start..index];
-            seq_new.push(window);
-            seq_new.push("\n".as_bytes());
-        } else {
-            index = seq_len;
-            let window = &seq_slice[start..index];
-            seq_new.push(window);
-        }
-        // line end
-        if index == seq_len {
-            break;
-        }
-    }
-    let seq_wrap = seq_new.concat();
-
-    Ok(seq_wrap)
-}
-
 pub fn write_record<W>(
     writer: &mut W,
     id: &[u8],
